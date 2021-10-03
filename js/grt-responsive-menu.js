d7a3766e6ac192eb3d917da871b9323ca69f541a
Abraham Víctor Zaragoza Rodríguez <avictor.zrodriguez@gmail.com>
2021-10-03 15:35:10 -0500

    Add files via upload

--- js/grt-responsive-menu.js
+++ js/grt-responsive-menu.js
@@ -13,7 +13,7 @@
     $(".grt-mobile-button").on("click", function () {
       $(this).toggleClass("grt-mobile-button-open");
       $("ul.grt-menu").toggleClass("open-grt-menu ");
-      $("html, body").toggleClass("body-overflow");
+      // $("html, body").toggleClass("body-overflow");
     });
     $("li.grt-dropdown").on("click", function (e) {
       $(this).toggleClass("active-dropdown");
@@ -21,19 +21,30 @@
   };
 })(jQuery);
 
+function closeMenu() {
+  let ulGrtMenu = document.querySelector("gtr-menu");
+  let ulGrtMobileButton = document.querySelector("grt-mobile-button");
+  // ulGrtMenu.classList.toggle("grt-mobile-button-close");
+  ulGrtMenu.classList.removeClass("active");
+  ulGrtMobileButton.classList.removeClass("active");
+}
+
 // Initialize and check for mobile
 $.fn.grtmobile();
 
-// // Add fixed class on scroll after 60px
-// $(window).scroll(function (e) {
-//   if ($(this).scrollTop() > 60) {
-//     $("header").addClass("scrolled");
-//   } else {
-//     $("header").removeClass("scrolled");
-//   }
-// });
+// Add fixed class on scroll after 60px
+$(window).scroll(function (e) {
+  if ($(this).scrollTop() > 60) {
+    $("header").addClass("scrolled");
+  } else {
+    $("header").removeClass("scrolled");
+  }
+});
 
 // Prevent a href clicks on dropdown category item
-// $("li.grt-dropdown > a").on("click", function (e) {
-//   // e.preventDefault();
-// });
+$("gtr-menu__li > a").on("click", function (e) {
+  // e.preventDefault();
+  // return true;
+  closeMenu();
+  console.log("prueba");
+});
